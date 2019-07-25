1b3d46eb82a0fc82301c866e2b08eaf2abc7f83f
Berend <berend.swennenhuis@trimm.nl>
2019-07-25 22:28:39 +0200

    Again: made stuff work!

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,13 @@
 bme280 bme;
 
 void setup() {
+  Serial.begin(115200);
   bme.init();
   // put your setup code here, to run once:
 }
 
 void loop() {
+  bme.printValues();
+  delay(2000);
   // put your main code here, to run repeatedly:
 }
